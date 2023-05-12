6c716a7649d7eefd5b2e8c1a756a4c6b04a05dd5
tiborauer <tibor.auer@gmail.com>
2023-05-13 00:22:51 +0100

    NEW - eddy

--- modules/preprocessing/reproa_eddy.m
+++ modules/preprocessing/reproa_eddy.m
@@ -1,116 +1,136 @@
-% mask
-%   fslmaths topup_fieldmap -Tmean topup_mean
-%   bet topup_mean topup_brain -n -m -f 0.2
-%   topup_brain_mask.nii
-% index
-%    dlmwrite('index.txt',ones(1,nVol),'delimiter',' ');
-% bvals
-%    dlmwrite('bvals',zeros(1,nVol),'delimiter',' ');
-% bvecs
-%    dlmwrite('bvecs',repmat([1;0;0],1,nVol),'delimiter',' ');
-% slspec
-%    slspec = unique(header{1}.sliceorder,'stable')-1;
-%    mbf = numel(header{1}.sliceorder)/numel(slspec);
-%    for b = 2:mbf
-%        slspec(:,b) = slspec(:,1)+(b-1)*size(slspec,1);
-%    end
-%    dlmwrite('slspec.txt',slspec,'delimiter',' ');
-%
-% eddy_cuda10.2 \
-%     --imain=$WDIR/sub-032301_ses-01_task-rest_acq-AP_run-01_bold.nii \
-%     --mask=$WDIR/topup_mask_mask.nii \
-%     --acqp=$WDIR/acquisition_parameters.txt \
-%     --index=$WDIR/index.txt \
-%     --bvecs=$WDIR/bvecs \
-%     --bvals=$WDIR/bvals \
-%     --topup=$WDIR/topup \
-%     --out=$WDIR/eddy-cuda-full \
-%     --niter=10 --fwhm=10,10,5,5,0,0,0,0,0,0 --repol \
-%     --slspec=$WDIR/slspec.txt --mporder=8 --s2v_niter=5 --s2v_lambda=1 --s2v_interp=trilinear \
-%     --estimate_move_by_susceptibility --mbs_niter=20 --mbs_lambda=5 --mbs_ksp=5 \
-%     --b0_only --data_is_shelled --verbose 
 function rap = reproa_eddy(rap,command,subj,run)
 
 switch command
     case 'doit'
-        
-        % Get nii filenames from stream
-        pelabels=[]; % for each volume, which acquisition does it come from
-        bvals=[];
-        bvecs=[];
-        allfns='';
-        for peind=1:2
-            diffinput=aas_getfiles_bystream(rap,'diffusion_session_phaseencode_direction',[subj run peind],'diffusion_data');
-            allfns=[allfns ' ' diffinput];
-            V=spm_vol(diffinput);
-            pelabels=[pelabels;peind*ones(length(V),1)];
-            bvals=[bvals load(aas_getfiles_bystream(rap,'diffusion_session_phaseencode_direction',[subj run peind],'bvals'))];
-            bvecs=[bvecs load(aas_getfiles_bystream(rap,'diffusion_session_phaseencode_direction',[subj run peind],'bvecs'))];
-        end;
+        sliceAxes = 'ijk';
+        pfxEddy = 'eddy';
+
+        % Detect CUDA
+        [s, w] = shell('nvidia-smi -q | grep CUDA','ignoreerror',true);
+        verCUDA = ~s && ~isempty(w);
+        if verCUDA, verCUDA = str2double(char(regexp(w,'(?<=:)[0-9\ \.]*','match'))); end
+
         % Output directory
-        dsess=aas_getpath_bydomain(rap,'diffusion_session',[subj run]);
-         
-        % Collect other files eddy needs
-        BETmask=aas_getfiles_bystream(rap,'diffusion_session',[subj run],'BETmask');
-        BETmask=BETmask(1,:); % I hate having to do this filtering, one day should refactor BET outputs
-        
-        acqparms=aas_getfiles_bystream(rap,'diffusion_session',[subj run],'topup_acquisition_parameters');
-        topup_output_fieldcoef=aas_getfiles_bystream(rap,'diffusion_session',[subj run],'topup_output_fieldcoef');        
-        [pth nme ext]=fileparts(topup_output_fieldcoef);
-        topup_output_rootfn=fullfile(pth,nme(1:end-10));
-        
-        % Write phase encode labels (rows in acq parms)
-        pelabelsfn=fullfile(dsess,'pelabels.txt');  
-        fid=fopen(pelabelsfn,'w');
-        fprintf(fid,'%d\n',pelabels);
-        fclose(fid);
-        
-        % Write bvals
-        bvalsfn=fullfile(dsess,'bvals');
-        fid=fopen(bvalsfn,'w');
-        fprintf(fid,'%f ',bvals);
-        fprintf(fid,'\n');
-        fclose(fid);
-        
-        % Write bvecs
-        bvecsfn=fullfile(dsess,'bvecs');
-        fid=fopen(bvecsfn,'w');
-        fprintf(fid,'%f ',bvecs(1,:));
-        fprintf(fid,'\n');
-        fprintf(fid,'%f ',bvecs(2,:));
-        fprintf(fid,'\n');
-        fprintf(fid,'%f ',bvecs(3,:));
-        fprintf(fid,'\n');
-        fclose(fid);
-        
-        
-        % Create merged file
-        mergedfn=fullfile(dsess,'allpe.nii');
-        cmd=['fslmerge -t ' mergedfn ' ' allfns];
-        [s w]=aas_runfslcommand(rap,cmd);
-        
+        localFolder = getPathByDomain(rap,rap.tasklist.currenttask.domain,[subj run]);
+
+        % Data
+        fnData = getFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'fmri','streamType','input');
+
+        % Mask
+        fnFM = char(getFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'dualpefieldmap'));
+        runFslCommand(rap,['fslmaths ' fnFM ' -Tmean ' strrep(fnFM,'fieldmap.nii','mean.nii')]);
+        runFslCommand(rap,['bet ' strrep(fnFM,'fieldmap.nii','mean.nii') ' ' strrep(fnFM,'fieldmap.nii','brain.nii') ' -n -m -f 0.2']);
+        fnMask = strrep(fnFM,'fieldmap.nii','brain_mask.nii');
+
+        % Acqisition parameters
+        fnAcq = getFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'acquisitionparameters');
+        tableTopup = dlmread(fnAcq{1});
+
+        % Index
+        fnAcqInd = fullfile(localFolder,'index.txt');
+        nVol = numel(spm_vol(fnData{1}));
+        load(char(getFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'fmri_header')),'header');
+        indTable = cellfun(@(p) ...
+                           min(find(arrayfun(@(r) ...
+                                             isequal((sliceAxes == p(1))*str2double(fliplr(regexprep(p,['[' sliceAxes ']'],'1'))),tableTopup(r,1:3)),...
+                                             1:size(tableTopup,1)))),...
+                           cellstr(header{1}.PhaseEncodingDirection));
+        dlmwrite(fnAcqInd,indTable*ones(1,nVol),'delimiter',' ');
+
+        % Topup
+        fnTopup = regexprep(char(getFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'fieldcoefficients')),'_fieldcoef.nii.*','');
+
+        % b
+        if hasStream(rap,rap.tasklist.currenttask.domain,[subj run],'bvals')
+            fnBVals = char(getFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'bvals'));
+        else
+            fnBVals = fullfile(localFolder,'bvals');
+            dlmwrite(fnBVals,zeros(1,nVol),'delimiter',' ');
+        end
+        if hasStream(rap,rap.tasklist.currenttask.domain,[subj run],'bvecs')
+            fnBVecs = char(getFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'bvecs'));
+        else
+            fnBVecs = fullfile(localFolder,'bvecs');
+            dlmwrite(fnBVecs,repmat([1;0;0],1,nVol),'delimiter',' ');
+        end
+
+        % eddy parameters
+        strQCParam = '';
+        % - binary
+        switch getSetting(rap,'wheretoprocess')
+            case 'auto'
+                strEddy = 'eddy';
+            case 'cpu'
+                strEddy = 'eddy_cpu';
+            case 'gpu'
+                binaries = cellstr(spm_select('List',fullfile(rap.directoryconventions.fsldir,'bin'),'^eddy_cuda.*'));
+                switch numel(binaries)
+                    case 0
+                        logging.error('No CUDA version of Eddy is detected in %s',fullfile(rap.directoryconventions.fsldir,'bin'));
+                    case 1
+                        strEddy = binaries{1};
+                    otherwise
+                        verEddy = sort(cellfun(@(b) sscanf(b,'eddy_cuda%f'), binaries));
+                        strEddy = sprintf('eddy_cuda%1.1f', verEddy(find(verEddy <= verCUDA,1,'last')));
+                end
+        end
+        % - mode
+        switch getSetting(rap,'mode')
+            case 'minimum'
+                strEddyParam = '-fwhm=0';
+            case 'medium'
+                strEddyParam = '--niter=7 --fwhm=10,0,0,0,0,0,0';
+            case 'extensive'
+                strEddyParam = '--niter=10 --fwhm=10,10,5,5,0,0,0,0,0,0';
+        end
+        % - movements
+        if getSetting(rap,'susceptibilitybymovement'), strEddyParam = [strEddyParam ' --estimate_move_by_susceptibility --mbs_niter=20 --mbs_lambda=5 --mbs_ksp=5']; end
+        if getSetting(rap,'slicetovolume')
+            doStV = true;
+            if strcmp(strEddy,'eddy_cpu')
+                logging.warning('slicetovolume correction is availabe only in the CUDA version');
+                doStV = false;
+            end
+            if doStV && ~verCUDA
+                logging.warning('no CUDA is detected');
+                doStV = false;
+            end
+            if doStV && ~isfield(header{1},'sliceorder')
+                logging.warning('sliceorder is not detected');
+                doStV = false;
+            end
+            if ~doStV, logging.warning('\t-> slicetovolume correction will be skipped');
+            else
+                fnSlSpec = fullfile(localFolder,'slspec.txt');
+                slspec = unique(header{1}.sliceorder,'stable')-1;
+                mbf = numel(header{1}.sliceorder)/numel(slspec);
+                for b = 2:mbf
+                    slspec(:,b) = slspec(:,1)+(b-1)*size(slspec,1);
+                end
+                dlmwrite(fnSlSpec,slspec,'delimiter',' ');
+                strEddyParam = [strEddyParam ' --slspec=' fnSlSpec ' --mporder=8 --s2v_niter=5 --s2v_lambda=1 --s2v_interp=trilinear'];
+                strQCParam = [strQCParam ' --slspec=' fnSlSpec];
+            end
+        end
+        % - data
+        if strcmp(rap.tasklist.currenttask.domain,'fmrirun'), strEddyParam = [strEddyParam ' --b0_only']; end
+
         % Now run eddy
-        outfn=fullfile(dsess,'eddy_output');       
-        cmd=[sprintf('eddy --imain=%s --mask=%s --acqp=%s --index=%s ',mergedfn,BETmask,acqparms,pelabelsfn)...
-             sprintf('--bvecs=%s --bvals=%s --topup=%s ',bvecsfn,bvalsfn,topup_output_rootfn)...
-             sprintf('--out=%s',outfn)];
-         
-        aas_log(rap,false,sprintf('Running %s',cmd));        
-         [s w]=aas_runfslcommand(rap,cmd);
-        if s
-             aas_log(rap,true,sprintf('Error %s',w));
-        end;
-        
-        output_list={'eddy_movement_rms','eddy_outlier_map','eddy_outlier_n_stdev_map','eddy_outlier_report','eddy_parameters','eddy_post_eddy_shell_alignment_parameters','eddy_rotated_bvecs','nii'};
-        
-        % Describe outputs
-        for outputind=1:length(output_list)
-            rap=aas_desc_outputs(rap,'diffusion_session',[subj run],'diffusion_data',[outfn '.' output_list{outputind}]);
-        end;
-       
-        rap=aas_desc_outputs(rap,'diffusion_session',[subj run],'bvals',bvalsfn);
-        rap=aas_desc_outputs(rap,'diffusion_session',[subj run],'bvecs',bvecsfn);
-        
+        cmd = sprintf('%s --imain=%s --mask=%s --acqp=%s --index=%s --bvecs=%s --bvals=%s --topup=%s --out=%s %s --repol --data_is_shelled --verbose', ...
+                      strEddy,fnData{1},fnMask,fnAcq{1},fnAcqInd,fnBVecs,fnBVals,fnTopup,fullfile(localFolder,pfxEddy),strEddyParam);
+
+        runFslCommand(rap,cmd);
+
+        % Outputs
+        putFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'fmri',spm_select('FPList',localFolder, ['^' pfxEddy '\.[^(eddy)]']));
+        putFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'movementparameters',fullfile(localFolder,[pfxEddy '.eddy_parameters']));
+
+        % QA (for DWI only)
+        if hasStream(rap,rap.tasklist.currenttask.domain,[subj run],'bvals')
+            fnFM = getFileByStream(rap,rap.tasklist.currenttask.domain,[subj run],'dualpefieldmap');
+            runFslCommand(rap,sprintf('eddy_quad %s --mask=%s --eddyParams=%s --eddyIdx=%s --bvals=%s --field=%s %s',...
+                                      fullfile(localFolder,pfxEddy),fnMask,fnAcq{1},fnAcqInd,fnBVals,fnFM{1},strQCParam(2:end)));
+        end
 end
 end
 
--- tests/usecases/test_LEMON_MRI_fmri.m
+++ tests/usecases/test_LEMON_MRI_fmri.m
@@ -1,6 +1,8 @@
 function test_LEMON_MRI_fmri(rap)
     rap.tasksettings.reproa_fromnifti_fieldmap.pattern = 'acq-SEfmapBOLDpre';
 
+    rap.tasksettings.reproa_eddy.mode = 'extensive';
+
     rap = processBIDS(rap);
 
     processWorkflow(rap);
