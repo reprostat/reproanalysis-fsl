6736550c9c1551dcea233cfaa055c2b3a73161b2
tiborauer <tibor.auer@gmail.com>
2023-05-16 21:43:30 +0100

    FIX - module naming

--- tests/usecases/test_LEMON_MRI_fmri.m
+++ tests/usecases/test_LEMON_MRI_fmri.m
@@ -1,7 +1,7 @@
 function test_LEMON_MRI_fmri(rap)
     rap.tasksettings.reproa_fromnifti_fieldmap.pattern = 'acq-SEfmapBOLDpre';
 
-    rap.tasksettings.reproa_eddy.mode = 'extensive';
+    rap.tasksettings.reproa_eddy_fmri.mode = 'extensive';
 
     rap = processBIDS(rap);
 
