4afabbcd834fb594a6934ed699e2fb746bab4dc0
tiborauer <tibor.auer@gmail.com>
2023-05-18 15:09:25 +0200

    ENH - branched example

--- tests/usecases/test_LEMON_MRI_fmri.m
+++ tests/usecases/test_LEMON_MRI_fmri.m
@@ -3,6 +3,13 @@ function test_LEMON_MRI_fmri(rap)
 
     rap.tasksettings.reproa_eddy_fmri.mode = 'extensive';
 
+    rap.tasksettings.reproa_coregextended(1).target = 'structural';
+    rap.tasksettings.reproa_coregextended(1).reorienttotemplate = 0;
+    rap.tasksettings.reproa_coregextended(2).target = 'meanfmri';
+    rap.tasksettings.reproa_coregextended(2).reorienttotemplate = 0;
+
+%    rap.tasksettings.reproa_segment.writenormalised.method = 'none';
+
     rap = processBIDS(rap);
 
     processWorkflow(rap);
