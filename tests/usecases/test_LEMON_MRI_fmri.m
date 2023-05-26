cb032e8dc7a7d58ae0673950d3c769f5d1348637
tiborauer <tibor.auer@gmail.com>
2023-05-26 22:48:03 +0200

    ENH - example

--- tests/usecases/test_LEMON_MRI_fmri.m
+++ tests/usecases/test_LEMON_MRI_fmri.m
@@ -1,14 +1,24 @@
 function test_LEMON_MRI_fmri(rap)
+    rap.options.parallelresources.walltime = 6;
+
     rap.tasksettings.reproa_fromnifti_fieldmap.pattern = 'acq-SEfmapBOLDpre';
 
     rap.tasksettings.reproa_eddy_fmri.mode = 'extensive';
 
     rap.tasksettings.reproa_coregextended(1).target = 'structural';
-    rap.tasksettings.reproa_coregextended(1).reorienttotemplate = 0;
     rap.tasksettings.reproa_coregextended(2).target = 'meanfmri';
-    rap.tasksettings.reproa_coregextended(2).reorienttotemplate = 0;
 
-%    rap.tasksettings.reproa_segment.writenormalised.method = 'none';
+    for b = 1:2
+        rap.tasksettings.reproa_coregextended(b).reorienttotemplate = 0;
+
+        rap.tasksettings.reproa_coregextended_t2(b).reorienttotemplate = 0;
+
+        rap.tasksettings.reproa_segment(b).writenormalised.method = 'none';
+
+        rap = renameStream(rap,sprintf('reproa_mask_segmentation_%05d',b),'input','reference','meanfmri');
+        rap = renameStream(rap,sprintf('reproa_mask_segmentation_%05d',b),'input','segmentations','native_segmentations');
+        rap.tasksettings.reproa_mask_segmentation(b).threshold = 'exclusive';
+    end
 
     rap = processBIDS(rap);
 
