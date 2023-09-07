a3f7082a8369c20a7a8a03562faa0a648ffa4da6
tiborauer <tibor.auer@gmail.com>
2023-09-07 03:31:03 +0200

    ENH - AROMA for test

--- tests/usecases/test_LEMON_MRI_fmri.m
+++ tests/usecases/test_LEMON_MRI_fmri.m
@@ -6,7 +6,11 @@ function test_LEMON_MRI_fmri(rap)
     rap.tasksettings.reproa_eddy_fmri.mode = 'extensive';
 
     rap.tasksettings.reproa_coregextended(1).target = 'structural';
+    rap = renameStream(rap,'reproa_coregextended_00001','input','append','brainmask');
     rap.tasksettings.reproa_coregextended(2).target = 'meanfmri';
+
+    rap = renameStream(rap,'reproa_aroma_00001','input','structural','reproa_fromnifti_structural_00001.structural');
+    rap = renameStream(rap,'reproa_aroma_00002','input','structural','reproa_coregextended_00002.structural');
 
     for b = 1:2
         rap.tasksettings.reproa_coregextended(b).reorienttotemplate = 0;
@@ -14,10 +18,23 @@ function test_LEMON_MRI_fmri(rap)
         rap.tasksettings.reproa_coregextended_t2(b).reorienttotemplate = 0;
 
         rap.tasksettings.reproa_segment(b).writenormalised.method = 'none';
+        rap.tasksettings.reproa_segment(b).writecombined = [0.5 0.1 0 0 0 0]; % ~ BET (GM and WM only)
 
         rap = renameStream(rap,sprintf('reproa_mask_segmentation_%05d',b),'input','reference','meanfmri');
         rap = renameStream(rap,sprintf('reproa_mask_segmentation_%05d',b),'input','segmentations','native_segmentations');
         rap.tasksettings.reproa_mask_segmentation(b).threshold = 'exclusive';
+
+        rap.tasksettings.reproa_smooth_fmri(b).FWHM = 4; % ~1.5 voxel size
+
+        rap = renameStream(rap,sprintf('reproa_aroma_%05d',b),'input','structural_brain',...
+                           sprintf('reproa_segment_%05d.structural',b));
+        rap.tasksettings.reproa_aroma(b).highpassfilter = 100; % in seconds
+        rap.tasksettings.reproa_aroma(b).includemovementparameters = [1 1 0; 1 1 0];
+        rap.tasksettings.reproa_aroma(b).includesegmentationsignal = ...
+            {'components-WMc1' 'components-WMc2' 'components-WMc3' 'components-WMc4' 'components-WMc5' ...
+             'components-CSFc1' 'components-CSFc2' 'components-CSFc3' 'components-CSFc4' 'components-CSFc5'};
+        rap.tasksettings.reproa_aroma(b).numberofdimensions = 100;
+        rap.tasksettings.reproa_aroma(b).componentregression = 'both';
     end
 
     rap = processBIDS(rap);
